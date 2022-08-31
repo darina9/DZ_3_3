64607ab6963fec3555c4cad010683aa89eaa6d59
darina9 <krastinadv@gmail.com>
2022-08-31 13:05:41 +0300

    Variant_2_zadanie_19

---
+++ Variant_2_zadanie_19/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+ if ((num/10000 == num%10) && ((num/1000)%10 == (num%100)/10))
+ {
+    Console.WriteLine("Это число палиндром");
+ }
+ else{
+    Console.WriteLine("Это число не палиндром");
+ }
