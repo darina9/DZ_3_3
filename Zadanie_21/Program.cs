47b591da2651e4d76694a5d880a2cd07929b7c0f
darina9 <krastinadv@gmail.com>
2022-08-31 13:10:07 +0300

    .\Zadanie_21\

---
+++ Zadanie_21/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.WriteLine("Введите координаты точки А :");
+ int x1 = Convert.ToInt32(Console.ReadLine());
+ int y1 = Convert.ToInt32(Console.ReadLine());
+ int z1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите координаты точки B :");
+ int x2 = Convert.ToInt32(Console.ReadLine());
+ int y2 = Convert.ToInt32(Console.ReadLine());
+ int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d  = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+
+Console.WriteLine(d);
+
