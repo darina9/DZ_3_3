76a9bfab2b058fbcfa98f72e2a70b973161bfad4
darina9 <krastinadv@gmail.com>
2022-08-31 13:16:44 +0300

    .\Zadanie_23\

---
+++ Zadanie_23/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Введите число n  :");
+int n = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+for(index=1;index<=n;index++)
+{
+    Console.WriteLine(Math.Pow(index,3));
+}
